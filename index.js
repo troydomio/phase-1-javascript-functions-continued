fbc7b3984d819386a62c0ba27804eed64b1acb58
Troy <troyakins99@yahoo.com>
2021-07-01 18:51:28 -0500

    commit tree a79ea2bc141a812fd3a6e0b15ed99e8584e2e312

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function saturdayFun(activity, noEntry="roller-skate"){
+    if(activity){return `This Saturday, I want to ${activity}!`} return `This Saturday, I want to ${noEntry}!`}
+    console.log();
+
+function mondayWork(activity1, activity2 = "go to the office"){
+    if(activity1){return `This Monday, I will ${activity1}.`;} return `This Monday, I will ${activity2}.`
+}
+
+function wrapAdjective(flair="*"){
+         let ok= function(adj){
+         return`You are ${flair}${adj}${flair}!`
+    }
+    return ok;
+}
+
+
+ console.log(wrapAdjective('*')('smart'))
+
